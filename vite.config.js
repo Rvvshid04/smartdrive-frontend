2f2b0db84105f4348b11824fb6906e2aacd81a5a
Rvvshid04 <raashid.arq@gmail.com>
2025-04-26 18:14:14 +0530

    Changes to config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/smartdrive-frontend/', 
   plugins: [
     react(),
     tailwindcss()  
